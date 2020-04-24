b92011acbc7f6e23d06b2bf8d0dffbdcc7820c95
msnmz <snmez.muharrem@gmail.com>
2020-04-25 01:48:33 +0200

    Creates Search component

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Input, Grid, Button } from 'semantic-ui-react';
+
+const Search = ({ onInputChange, placeholder, onSearch }) => {
+  const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = e => {
+    setSearchText(e.target.value);
+    onInputChange && onInputChange(e);
+  }
+
+  const handleSearchClick = () => {
+    setLoading(true);
+    onSearch && onSearch(searchText, setLoading);
+  }
+
+  return (
+    <>
+      <Grid padded>
+        <Grid.Column style={{ paddingRight: 0 }} mobile={14} tablet={14} computer={14}>
+          <Input fluid loading={loading} value={searchText} onChange={handleInputChange} placeholder={placeholder} />
+        </Grid.Column>
+        <Grid.Column mobile={2} tablet={2} computer={2}>
+          <Button basic icon='search' onClick={handleSearchClick} />
+        </Grid.Column>
+      </Grid>
+    </>
+  );
+}
+
+export default Search;
